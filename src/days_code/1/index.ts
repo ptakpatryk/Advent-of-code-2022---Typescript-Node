d32cfa97351080c5ec45b9212dd8f29d290ec0ab
ptakpatryk <ptaszqq@gmail.com>
2022-12-01 12:27:27 +0000

    day 1 (part I&II): solved

--- src/days_code/1/index.ts
+++ src/days_code/1/index.ts
@@ -1,3 +1,29 @@
-export default () => {
-  console.log('day 1');
+import fs from 'fs';
+
+export const partOne = () => {
+  const caloriesList = caloriesSumList();
+
+  return Math.max(...caloriesList);
 };
+
+export const partTwo = () => {
+  const caloriesList = caloriesSumList();
+  // Sort descending
+  caloriesList.sort((a, b) => b - a);
+
+  const topThree = caloriesList.slice(0, 3);
+
+  return topThree.reduce((acc, curr) => acc += curr, 0);
+};
+
+function caloriesSumList() {
+  const list = fs.readFileSync('./public/day_1_input.txt', 'utf8');
+  const calories = list.split('\n\n');
+  const elvesCalories = calories.map(elfFood => elfFood.split('\n'));
+
+  // PARSE ARRAY TO NUMBERS AND CLEAN NULLS
+  const elvesCaloriesNums = elvesCalories.map(elfList => elfList.map(el => parseInt(el)).filter(Boolean));
+
+  // Sum per elf
+  return elvesCaloriesNums.map(elfList => elfList.reduce((caloriesSum, curr) => caloriesSum += curr, 0));
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
 import * as dayOne from './days_code/1';
 
-dayOne.default();
+// ***** DAY 1
+// * PART 1
+const dayOnePartOneSolution = dayOne.partOne();
+console.log(`Day 1 (part I) - Elf carrying the most calories is carrying ${dayOnePartOneSolution} calories.`);
+
+const dayOnePartTwoSolution = dayOne.partTwo();
+console.log(`Day 1 (part II) - Three elves carrying the most calories are carrying ${dayOnePartTwoSolution} calories.`);
+
